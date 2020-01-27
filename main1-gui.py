b3f01db3b5ce295c3f7da04058eb3a5647711d93
Anders Demant van der Weide <antra@antra.dk>
2020-01-27 21:34:51 +0100

    Updated with basic GU

---
+++ main1-gui.py
@@ -0,0 +1,76 @@
+import tkinter
+from tkinter import ttk, StringVar
+from tkinter.filedialog import askopenfilename
+import pandas as pd
+import numpy as np
+
+
+class GUI:
+
+    def __init__(self, window):
+        # 'StringVar()' is used to get the instance of input field
+        self.input_text = StringVar()
+        self.input_text1 = StringVar()
+        self.path = ''
+        self.delimiter = ''
+
+        window.title("Simple CSV Splitter")
+        window.resizable(0, 0)  # this prevents from resizing the window
+        window.geometry("700x300")
+
+        ttk.Button(window, text="CSV File",
+                   command=lambda: self.set_csv_path()
+                   ).grid(row=0, ipadx=5, ipady=5)
+        ttk.Entry(window,
+                  textvariable=self.input_text,
+                  width=70).grid(row=0, column=1, ipadx=1, ipady=1)
+
+        #ttk.Button(window, text="Enova File", command=lambda: self.set_path_Enova_field()).grid(row=1, ipadx=5, ipady=15)
+        #ttk.Entry(window, textvariable=self.input_text1, width=70).grid(row=1, column=1, ipadx=1, ipady=1)
+
+        ttk.Label(window, text="Specify delimiter:").grid(
+            row=1, ipadx=5, ipady=15)
+        self.txt_delimiter = ttk.Entry(
+            window, textvariable=self.input_text1, width=3)
+        self.txt_delimiter.grid(row=1, column=1, ipadx=1, ipady=1, sticky="w")
+        self.txt_delimiter.insert(0, ';')
+
+        ttk.Button(window, text="Run", command=self.run).grid(
+            row=2, column=1, ipadx=5, ipady=5, sticky="e")
+
+        ttk.Button(window, text="Quit", command=self.quit).grid(
+            row=2, column=2, ipadx=5, ipady=5, sticky="e")
+
+    def set_csv_path(self):
+        self.path = askopenfilename()
+        self.input_text.set(self.path)
+
+    def run(self):
+        delimiter = self.txt_delimiter.get()
+        if delimiter not in [';', ',', '|']:
+            delimiter = ';'
+        path = self.get_csv_path()
+        df = pd.read_csv(path, delimiter=delimiter)
+        groups = df.groupby(np.arange(len(df.index)) / 10000)
+        for (frameno, frame) in groups:
+            filename = groups.get_group(frameno).values[0][0].replace(':', '')
+            frame.to_csv(f'{filename}.csv', sep=delimiter,
+                         header=True, index=False, encoding="ISO-8859-1")
+
+    def get_csv_path(self):
+        """Function provides the CSV full file path."""
+        return self.path
+
+    def get_delimiter(self):
+        """Function provides the delimiter."""
+        return self.delimiter
+
+    def quit(self):
+        """Function to end the execution."""
+        window.destroy()
+
+
+if __name__ == '__main__':
+    window = tkinter.Tk()
+    gui = GUI(window)
+    window.mainloop()
--- main1.py
+++ main1.py
@@ -1,70 +1,13 @@
 import pandas as pd
 import numpy as np
-import os
-import tkinter as tk
-from tkinter.filedialog import askopenfilename
-from functools import partial
 
-filepath = ''
+delimiter = ';'
+org_file = 'test.csv'
+df = pd.read_csv(org_file, delimiter=delimiter)
 
-
-def open_file():
-    """Open a file for reading"""
-    filepath = askopenfilename(
-        filetypes=[("CSV Files", "*.csv"), ("All Files", "*.*")]
-    )
-    if not filepath:
-        return
-    print(filepath)
-    lbl_csv_file['text'] = "Filename: " + str(os.path.basename(filepath))
-    filepath = filepath
-
-
-def run(filepath):
-    print(filepath)
-    delimiter = ent_delimiter['text']
-    if delimiter not in [';', ',', '|']:
-        delimiter = ';'
-    print(delimiter)
-    df = pd.read_csv(filepath, delimiter=delimiter)
-    print(df)
-    groups = df.groupby(np.arange(len(df.index)) / 10000)
-    for (frameno, frame) in groups:
-        filename = groups.get_group(frameno).values[0][0].replace(':', '')
-        frame.to_csv(f'{filename}.csv', sep=delimiter,
-                     header=True, index=False, encoding="ISO-8859-1")
-
-
-window = tk.Tk()
-window.title("Simple CSV Splitter")
-#window.rowconfigure(3, minsize=50, weight=1)
-#window.columnconfigure(1, minsize=50, weight=1)
-frm_form = tk.Frame(relief=tk.SUNKEN, borderwidth=3)
-frm_form.pack()
-lbl_csv_file = tk.Label(master=frm_form, text="Select CSV file...")
-btn_csv_file = tk.Button(
-    master=frm_form, text="Open", command=open_file)
-lbl_csv_file.grid(row=0, column=0, sticky="e")
-btn_csv_file.grid(row=0, column=2, sticky="e")
-lbl_delimiter = tk.Label(master=frm_form, text="Specify delimiter:")
-ent_delimiter = tk.Entry(master=frm_form, width=3)
-lbl_delimiter.grid(row=1, column=0, sticky="e")
-ent_delimiter.grid(row=1, column=1)
-ent_delimiter.insert(tk.END, ';')
-
-btn_run = tk.Button(
-    master=frm_form, text="Run", command=partial(run, filepath))
-btn_run.grid(row=2, column=2, sticky="e")
-
-#delimiter = ';'
-#org_file = 'test.csv'
-#df = pd.read_csv(org_file, delimiter=delimiter)
-
-#groups = df.groupby(np.arange(len(df.index))/10000)
-# for (frameno, frame) in groups:
-#filename = groups.get_group(frameno).values[0][0].replace(':', '')
-#frame.to_csv("%s.csv" % filename, sep=delimiter, header=True, index=False, encoding="ISO-8859-1")
-# frame.to_csv(f'{filename}.csv', sep=delimiter, header=True,
-#            index=False, encoding="ISO-8859-1")
-
-window.mainloop()
+groups = df.groupby(np.arange(len(df.index))/10000)
+for (frameno, frame) in groups:
+    filename = groups.get_group(frameno).values[0][0].replace(':', '')
+    #frame.to_csv("%s.csv" % filename, sep=delimiter, header=True, index=False, encoding="ISO-8859-1")
+    frame.to_csv(f'{filename}.csv', sep=delimiter, header=True,
+                 index=False, encoding="ISO-8859-1")
